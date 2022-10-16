db469784670015c5acd6b45dc12b38836cf94db2
elenapelaez101 <elena.pelaezsanchez@usp.ceu.es>
2022-10-16 13:01:11 +0200

    creacion de main

--- funciones.py
+++ funciones.py
@@ -23,19 +23,3 @@ def sumaryañadir(lis):
     suma = sum(lis)
     lis.insert(0, suma)
     return lis
-
-
-def modificar(listainicial):
-    lista =borrdup(listainicial)
-    lista = ordenar(lista)
-    lista = eliminarimpar(lista)
-    lista = sumaryañadir(lista)
-    return lista
-
-
-
-
-lista = [3,5,7,4,6,8,5,3,4,6,8,9,3,4,3,5]
-nueva_lista = modificar(lista)
-print(nueva_lista)
-print( nueva_lista[0] == sum(nueva_lista[1:]) )
