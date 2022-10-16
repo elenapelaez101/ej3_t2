00dbb9bab156a73c46ba5e6b95d02c1614698e4c
elenapelaez101 <elena.pelaezsanchez@usp.ceu.es>
2022-10-16 12:52:58 +0200

    casi finalizado

--- ejercicio.py
+++ ejercicio.py
@@ -36,4 +36,6 @@ def modificar(listainicial):
 
 
 lista = [3,5,7,4,6,8,5,3,4,6,8,9,3,4,3,5]
-modificar(lista)
+nueva_lista = modificar(lista)
+print(nueva_lista)
+print( nueva_lista[0] == sum(nueva_lista[1:]) )
