26b277cda2b334a4ae174dfa696eb048083f4641
elenapelaez101 <elena.pelaezsanchez@usp.ceu.es>
2022-10-16 12:59:11 +0200

    segmentacion

---
+++ main.py
@@ -0,0 +1,14 @@
+def modificar(listainicial):
+    lista =borrdup(listainicial)
+    lista = ordenar(lista)
+    lista = eliminarimpar(lista)
+    lista = sumaryañadir(lista)
+    return lista
+
+
+
+
+lista = [3,5,7,4,6,8,5,3,4,6,8,9,3,4,3,5]
+nueva_lista = modificar(lista)
+print(nueva_lista)
+print( nueva_lista[0] == sum(nueva_lista[1:]) )
